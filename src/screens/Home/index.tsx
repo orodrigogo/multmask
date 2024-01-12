3e72ec1a03def213a9f59d5eb18b48e7c1b1fcb2
rodrigo <rodrigo.rgtic@gmail.com>
2024-01-12 19:23:52 -0300

    add masks

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,18 +2,36 @@ import { useState } from "react"
 import { View } from "react-native"
 
 import { styles } from "./styles"
-
 import { Input } from "@/components/Input"
+
+import { cpfApplyMask, cnpjApplyMask } from "@/utils/masks"
 
 export function Home() {
   const [pix, setPix] = useState("")
+
+  function applyMask(input: string) {
+    const onlyNumbers = input.replace(/\D/g, "")
+
+    if (onlyNumbers.length === 11) {
+      const cpf = cpfApplyMask(onlyNumbers)
+      return setPix(cpf)
+    }
+
+    if (onlyNumbers.length === 14) {
+      const cnpj = cnpjApplyMask(onlyNumbers)
+      return setPix(cnpj)
+    }
+
+    const nameOrPixKey = input.replace(/[^a-zA-Z0-9 ]/g, "")
+    setPix(nameOrPixKey)
+  }
 
   return (
     <View style={styles.container}>
       <Input.Container>
         <Input.Field
           placeholder="Nome, CPF/CNPJ ou chave Pix"
-          onChangeText={setPix}
+          onChangeText={applyMask}
           value={pix}
         />
 
